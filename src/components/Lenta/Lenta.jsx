b627f8bddf0a9ed95de6775e1fc23e9d3d7d6dc2
Host <paklin.2020@gmail.com>
2020-03-19 03:57:35 +0500

    using array for data

--- src/components/Lenta/Lenta.jsx
+++ src/components/Lenta/Lenta.jsx
@@ -3,18 +3,27 @@ import Event from './Event/Event.jsx';
 import s from "./Lenta.module.css";
 
 const Lenta = () => {
-	let str = ("Съешь ещё этих сладких французских булок и выпей чаю ").repeat(23);
+	let str = "Съешь ещё этих сладких французских булок и выпей чаю ";
+	let eventInfo = str.repeat(23);
+
+	let Events = []
+	for (let i = 0; i < 25; i++) {
+		Events.push(
+			{ name: `Мероприятие ${i}`, 
+			  type: `Тип мероприятия ${i}`, 
+			  info: eventInfo }
+		);
+	}
+
+	let EventsJSX = Events.map(event => {
+		return <Event name={event.name} type={event.type} info={event.info} />
+	});
 
 	return (
 		<div className={s.Lenta}>
 			<div className={`${s.head}`}>Лента событий</div>
 			<div className={`${s.overflowX}`}>
-				<Event name="Мероприятие 1" type="тип мероприятия 1" info={str}/>
-				<Event name="Мероприятие 2" type="тип мероприятия 2" info={str}/>
-				<Event name="Мероприятие 3" type="тип мероприятия 3" info={str}/>
-				<Event name="Мероприятие 4" type="тип мероприятия 4" info={str}/>
-				<Event name="Мероприятие 5" type="тип мероприятия 5" info={str}/>
-				<Event name="Мероприятие 6" type="тип мероприятия 6" info={str}/>
+				{EventsJSX}
 			</div>
 		</div>
 	);
